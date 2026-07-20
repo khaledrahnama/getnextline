cb1c73d031c46058165345429f8a6587a99d8c4c
Khaledrahnama <khaledrahnama@users.noreply.github.com>
2026-07-21 01:08:13 +0200

    tests

---
+++ checker.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khaledrahnama <khaledrahnama@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/21 00:57:09 by khaledrahna       #+#    #+#             */
+/*   Updated: 2026/07/21 01:07:38 by khaledrahna      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc > 1)
+		fd = open(argv[1], O_RDONLY);
+	else
+		fd = 0;
+	if (fd < 0)
+	{
+		printf("Error opening file\n");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	if (argc > 1)
+		close(fd);
+	return (0);
+}
