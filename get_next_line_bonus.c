0838ace5f88af0ce3dbfa1f9eefe0406fd529f9e
Khaledrahnama <khaledrahnama@users.noreply.github.com>
2026-07-21 00:35:58 +0200

    fix : timeout issue for long lines

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -3,26 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khaledrahnama <khaledrahnama@student.42.fr> +#+  +:+       +#+       */
+/*   By: khaledrahnama <khaledrahnama@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 00:00:00 by khaledrahna       #+#    #+#             */
-/*   Updated: 2026/07/18 00:00:00 by khaledrahna      ###   ########.fr       */
+/*   Updated: 2026/07/21 00:35:26 by khaledrahna      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
+static char	*grow_stash(char *stash, size_t used_len, size_t needed_len,
+		size_t *capacity)
+{
+	char	*bigger;
+	size_t	new_capacity;
+	size_t	i;
+
+	if (needed_len < *capacity)
+		return (stash);
+	new_capacity = *capacity;
+	if (new_capacity < 1)
+		new_capacity = 1;
+	while (new_capacity <= needed_len)
+		new_capacity = new_capacity * 2;
+	bigger = malloc(new_capacity);
+	if (!bigger)
+		return (NULL);
+	i = 0;
+	while (i < used_len)
+	{
+		bigger[i] = stash[i];
+		i++;
+	}
+	free(stash);
+	*capacity = new_capacity;
+	return (bigger);
+}
+
 static char	*read_and_append(int fd, char **stash)
 {
 	char	*buffer;
-	char	*temp;
 	ssize_t	bytes_read;
+	size_t	len;
+	size_t	capacity;
+	size_t	i;
 
+	if (ft_strchr(*stash, '\n'))
+		return (*stash);
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
+	len = ft_strlen(*stash);
+	capacity = len + 1;
 	bytes_read = 1;
-	while (!ft_strchr(*stash, '\n') && bytes_read > 0)
+	while (bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
@@ -32,15 +66,25 @@ static char	*read_and_append(int fd, char **stash)
 			*stash = NULL;
 			return (NULL);
 		}
+		if (bytes_read == 0)
+			break ;
 		buffer[bytes_read] = '\0';
-		temp = ft_strjoin(*stash, buffer);
-		free(*stash);
-		*stash = temp;
+		*stash = grow_stash(*stash, len, len + (size_t)bytes_read, &capacity);
 		if (!*stash)
 		{
 			free(buffer);
 			return (NULL);
 		}
+		i = 0;
+		while (i < (size_t)bytes_read)
+		{
+			(*stash)[len + i] = buffer[i];
+			i++;
+		}
+		len = len + (size_t)bytes_read;
+		(*stash)[len] = '\0';
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
 	free(buffer);
 	return (*stash);
