d8cc756b898337887287c6056aded73c594508da
Khaledrahnama <khaledrahnama@users.noreply.github.com>
2026-07-23 21:08:30 +0200

    update

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
+/*   By: khaledrahnama <khaledrahnama@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 00:00:00 by khaledrahna       #+#    #+#             */
-/*   Updated: 2026/07/21 17:00:00 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/07/23 21:08:17 by khaledrahna      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
